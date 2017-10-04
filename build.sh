f6c53f87cc07968c1778a1d17757b08e4ad681c1
Tanner <gasquakestudios@gmail.com>
2017-10-03 22:11:28 -0500

    eon oct3,2017

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
-arm-none-eabi-gcc -mcpu=arm1176jzf-s -O0 -fpic -ffreestanding -c start.s -Wall
-arm-none-eabi-gcc -mcpu=arm1176jzf-s -O0 -fpic -ffreestanding -std=gnu99 -nostdlib -c ves_main.c -Wall
+arm-none-eabi-gcc -mcpu=arm1176jzf-s -mfloat-abi=softfp -mfpu=neon -O2 -fpic -ffreestanding -c start.s -Wall
+arm-none-eabi-gcc -mcpu=arm1176jzf-s -mfloat-abi=softfp -mfpu=neon -O2 -fpic -ffreestanding -std=gnu99 -nostdlib -c ves_main.c -Wall
 arm-none-eabi-ld start.o ves_main.o -T link.ld -o ves.elf
 arm-none-eabi-objcopy ves.elf -O binary kernel.img
-cp kernel.img /media/tanner/3437-3234/
+cp kernel.img /media/tanner/*/
